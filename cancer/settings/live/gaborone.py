50d59bb8d5b2ee991e13b0fc147bb1f2abe25ef6
ckgathi <ckgathi@gmail.com>
2019-05-13 02:25:36 +0200

    fix site

--- cancer/settings/live/gaborone.py
+++ cancer/settings/live/gaborone.py
@@ -2,7 +2,7 @@ from ...sites import get_site_id
 from .base_live import *
 
 # for django.contrib.sites
-SITE_ID = get_site_id('francistown')
+SITE_ID = get_site_id('gaborone')
 
 WSGI_APPLICATION = 'cancer.wsgi.gaborone.application'
 
