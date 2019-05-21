e2ec47f72a16b192df163fa0e1e24ffb439dc70e
ckgathi <ckgathi@gmail.com>
2019-05-21 04:25:25 +0200

    do web conf files

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,10 +1,17 @@
+# cancer-live gunicorn.conf
 import os
 
-SOURCE_ROOT = os.path.expanduser('~/source')
-APP_NAME = 'cancer'
+from pathlib import Path
+
+SOURCE_ROOT = str(Path(os.path.dirname(os.path.abspath(__file__))).parent)
+
+errorlog = os.path.join(SOURCE_ROOT, 'log/cancer-gunicorn-error.log')
+accesslog = os.path.join(SOURCE_ROOT, 'log/cancer-gunicorn-access.log')
+loglevel = 'debug'
+pidfile = os.path.join(SOURCE_ROOT, 'run/cancer-live.pid')
+
+workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
+
+raw_env = ['DJANGO_SETTINGS_MODULE=cancer.settings']
 
 bind = "127.0.0.1:9000"
-errorlog = os.path.join(SOURCE_ROOT, f'{APP_NAME}/logs/gunicorn-error.log')
-accesslog = os.path.join(SOURCE_ROOT, f'{APP_NAME}/logs/gunicorn-access.log')
-loglevel = 'debug'
-workers = 2
