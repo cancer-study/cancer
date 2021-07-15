1f081bb0fc0e3abbcff0adc83839ebaa314cd97e
ame diphoko <diphokoame@gmail.com>
2021-07-15 09:02:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cancer',
-    version='0.1.4',
+    version='0.1.5',
     author=u'BHP Software Engineering & Data Management Center',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
